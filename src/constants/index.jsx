e1972d95ac18bd67142caa081920a8d433afb68a
Emmanuella Amonoo <amonooemma2@gmail.com>
2024-07-09 11:58:15 +0000

    first commit

---
+++ src/constants/index.jsx
@@ -0,0 +1,25 @@
+const K = {
+    NAVLINKS: [
+        {
+            name: "Dashboard",
+            path: "/dashboard"
+        },
+        {
+            name: "Preview",
+            path: "/preview"
+        },
+       
+        {
+            name: "Log In",
+            path: "/logIn"
+        },
+        {
+            name: "Sign Up",
+            path: "/signUp"
+        },
+    ],
+}   
+
+
+
+export default K;
