22b35144fab071981c5b20f8b106054b8f56d317
emmie-j <attachiegrace408@gmail.com>
2024-07-12 14:57:32 +0000

    fix tailwind issue

--- src/pages/logIn/index.jsx
+++ src/pages/logIn/index.jsx
@@ -3,49 +3,9 @@
 const LogIn = () => {
 
   return (
-  
-      <div className="flex h-screen">
-        <div className="w-1/2 flex items-center justify-center bg-lightBlue">
-          <div className="w-full max-w-sm space-y-4 p-8">
-            <h1 className="text-5xl font-semibold">Sign in</h1>
-            <form className="space-y-4">
-              <div>
-                <label className="block text-sm font-medium text-gray-700">Email address</label>
-                <input
-                  type="email"
-                  placeholder="Email address"
-                  className="mt-1 block w-full border border-gray-300 rounded-md p-3"
-                />
-              </div>
-              <div>
-                <label className="block text-sm font-medium text-gray-700">Password</label>
-                <input
-                  type="password"
-                  placeholder="Password"
-                  className="mt-1 block w-full border border-gray-300 rounded-md p-3"
-                />
-              </div>
-              <button className="w-full bg-[#A5045B] text-white py-2 px-4 rounded shadow-sm hover:bg-[#D00855]">
-                Log in
-              </button>
-              <div className="flex items-center my-4">
-                <hr className="w-full border-gray-300" />
-                <span className="mx-2 text-gray-500">or</span>
-                <hr className="w-full border-gray-300" />
-              </div>
-              <button className="w-full bg-white border border-gray-300 text-gray-700 py-2 px-4 rounded shadow-sm hover:bg-gray-50 flex items-center justify-center">
-                <img src="" alt="Google logo" className="w-5 h-5 mr-2" />
-                Sign in with Google
-              </button>
-            </form>
-          </div>
-        </div>
-        <div className="w-1/2">
-          <img src="" alt="sign up and in background" className="object-cover w-full h-full" />
-        </div>
-      </div>
-    );
-  };
-  
+    <div>Login</div>
+  );
+};
+
 
 export default LogIn
--- src/pages/signUp/index.jsx
+++ src/pages/signUp/index.jsx
@@ -1,41 +1,95 @@
+import { useForm } from 'react-hook-form';
 
 
 const SignUp = () => {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+
   return (
-    <div className="flex h-screen">
-      <div className="w-1/2 flex items-center justify-center bg-lightBlue">
-        <div className="w-full max-w-sm space-y-4 p-8">
-          <h1 className="text-4xl font-semibold">Create Account</h1>
-          <form className="space-y-4">
-            <div>
-              <label className="block text-sm font-medium text-gray-700">Email address</label>
-              <input
-                type="email"
-                placeholder="Email address"
-                className="mt-1 block w-full border border-gray-300 rounded-md p-3"
-              />
-            </div>
-            <button className="w-full bg-[#A5045B] text-white py-2 px-4 rounded shadow-sm hover:bg-[#D00855]">
-              Sign Up
-            </button>
-            <div className="flex items-center my-4">
-              <hr className="w-full border-gray-300" />
-              <span className="mx-2 text-gray-500">or</span>
-              <hr className="w-full border-gray-300" />
-            </div>
-            <button className="w-full bg-white border border-gray-300 text-gray-700 py-2 px-4 rounded shadow-sm hover:bg-gray-50 flex items-center justify-center">
-              <img src={google} alt="Google logo" className="w-5 h-5 mr-2" />
-              Sign in with Google
-            </button>
-          </form>
+    <form className='flex w-full h-screen ' onSubmit={handleSubmit(onSubmit)}>
+      <div className="w-1/2 h-screen bg-black">
+        <img src="img_avatar2.png" alt="Avatar" className="avatar" />
+      </div>
+      <div className='flex flex-col w-1/2'>
+        <div className="flex gap-x-6 w-full">
+          <label htmlFor="firstName" className="font-semibold">First name</label>
+          <input
+            id="firstName"
+            type="text"
+            placeholder="Enter your first name" className='border-2 p-2 rounded-md'
+            {...register('firstName', {
+              required: 'Firstname is required'
+              //minlength: 2
+            })}
+          />
+          {errors.firstName && (
+            <p className='text-blue-700'>{errors.firstName.message}</p>
+          )}
         </div>
+
+
+        <div className=''>
+          <label htmlFor="lastName" className="font-semibold">Lastname</label>
+          <input
+            id="lastName"
+            type="text"
+            placeholder="Enter your last name" className='border-2 p-2 rounded-md'
+            {...register('lastName', {
+              required: 'Lastname is required'
+              //minlength: 8
+            })}
+          />
+          {errors.lastname && (<p className='text-red-500'>errors.lastName.message</p>)}
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            id='email'
+            type="text" />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            id='password'
+            type="password" />
+
+
+
+
+        </div>
+
+
+        <button type="submit" className='bg-transparent text-white w-full p-2 rounded-md font-semibold'>
+          Signup
+        </button>
+
+
+        {/* <label>
+          <input
+            type="checkbox" checked name="remember" />
+          Remember me
+        </label> */}
+        <div>
+          <p>Already have an account?</p>
+          {/* <Link to="/login">
+        Login
+        </Link> */}
+        </div>
+
+
+        <div className="container" >
+          <button type="button" className="cancelbtn">Cancel</button>
+          <span className="">Forgot <a href="#">password?</a></span>
+        </div>
+       
       </div>
-      <div className="w-1/2">
-        <img src={signUpImage} alt="sign up and in background" className="object-cover w-full h-full" />
-      </div>
-    </div>
+    </form>
   );
 };
+
 
 export default SignUp;
 
