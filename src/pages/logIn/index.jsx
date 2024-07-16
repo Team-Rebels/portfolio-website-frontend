cf20fade6fc2724de645597e5ca3535e2b2e357c
emmie-j <attachiegrace408@gmail.com>
2024-07-16 09:32:43 +0000

    login page added

--- src/pages/logIn/index.jsx
+++ src/pages/logIn/index.jsx
@@ -1,9 +1,138 @@
+import { Link, useNavigate } from "react-router-dom"
+import { useForm } from "react-hook-form"
+import { apiLogin } from "../../services/auth";
+import { useState } from 'react';
+import background from "../../assets/images/background.avif"
+import googlelogo from "../../assets/images/googlelogo.png"
 
 
 const LogIn = () => {
+  const [isSubmitting, setIsSubmitting] = useState(false)
+  const navigate = useNavigate
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm();
+
+  const onSubmit = async (data) => {
+    console.log(data);
+    try {
+      const res = await apiLogin({
+        email: data.email,
+        password: data.password
+      });
+      console.log("Response", res.data);
+      navigate("/dashboard")
+
+
+
+    } catch (error) {
+      console.log(error);
+
+    } finally {
+      setIsSubmitting(false)
+    }
+  }
 
   return (
-    <div>Login</div>
+
+    <div className="">
+      <div className="flex">
+        <div className="w-1/2 h-screen">
+
+          <img src={background} alt="Login image" className="bg-cover bg-center" />
+        </div>
+
+
+        <div className="w-1/2">
+          <div className="flex flex-col justify-center text-left ">
+            <h3 className="font-extrabold text-center">Welcome Back.</h3>
+            <p></p>
+
+            <form onSubmit={handleSubmit(onSubmit)} className="flex justify-center">
+              <div className="flex flex-col space-y-1">
+                <label
+                  htmlFor="email"
+                  className="font-semibold text-[#1826ef] mb-0">
+                  Email
+                </label>
+                <input
+                  type="text"
+                  id="email"
+                  className="h-10 w-1/2 px-2 py-1 border-blue-500 border-2 rounded-lg "
+                  {
+                  ...register("email", { required: "Enter your email !" })
+                  }
+                />
+                {errors.email && (<p className="text-red-500">{errors.email.message}</p>)}
+
+                <label
+                  htmlFor="password"
+                  className="font-semibold text-[#1826ef] mb-0 "
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  id="password"
+                  className="h-10 w-1/2 px-2 py-1 border-blue-500 border-2 rounded-lg"
+                  {
+                  ...register("password", { required: "Enter your password !" })
+                  }
+                />
+                {errors.password && (<p className="text-red-500">{errors.password.message}</p>)}
+                <div className=" m-10 ml-16">
+                  <button
+                    type="submit"
+                    className="mt-10 mb-5 h-8 w-40 bg-white text-blue-500  border-blue-500 border-2 rounded-lg hover:bg-[#a1739b]">
+
+                    {isSubmitting ? "Loading..." : "Login"}
+                  </button>
+
+                  <div className="flex items-center">
+                    <hr className="flex-1 border-gray-300" />
+                    <span className="mx-2 text-gray-600">or</span>
+                    <hr className="flex-1 border-gray-300" />
+                  </div>
+
+                  <button className="flex items-center justify-center h-10 border border-blue-500 rounded-lg text-gray-600 hover:bg-gray-100 transition duration-200 w-1/2">
+                    <img className="w-5 h-5 mr-2" src={googlelogo} alt="Google logo" />
+                  </button>
+
+
+                </div>
+              </div>
+            </form>
+
+          </div>
+          <div className=" inline-block align-baseline">
+            <p>Not registered?</p>
+
+            <div className="text-justify">
+              <span className="underline">Forgot password?</span>
+            </div>
+
+            <div className=" font-normal">
+              <Link to="/signup" className="underline font-bold">
+                Sign Up
+              </Link>
+            </div>
+          </div>
+
+
+        </div>
+
+
+      </div>
+      </div>
+    
+
+
+
+
+
   );
 };
 
--- src/pages/signUp/index.jsx
+++ src/pages/signUp/index.jsx
@@ -1,4 +1,6 @@
 import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+import signup from '../../assets/images/signup.svg'
 
 
 const SignUp = () => {
@@ -9,84 +11,118 @@ const SignUp = () => {
 
 
   return (
-    <form className='flex w-full h-screen ' onSubmit={handleSubmit(onSubmit)}>
-      <div className="w-1/2 h-screen bg-black">
-        <img src="img_avatar2.png" alt="Avatar" className="avatar" />
+
+    <div className='grid grid-cols-2 h-screen w-full'>
+      <div className='flex items-center bg-primary/60'>
+        
+        <div className='mr-'>
+          {<img src={signup} alt="Signup" className="w-96 h-[550px] mt-20" />}
+        </div>
+
+        <div>
+          <form onSubmit={handleSubmit(onSubmit)} className='flex items-center justify-center h-[550px] w-96 bg-blue-400 rounded-lg' >
+
+            <div className="">
+
+            </div>
+
+            <div className='flex flex-col'>
+              <div className="flex gap-x-6 w-15 mt-5  text-white">
+                <label htmlFor="firstName" className="font-semibold">First name :</label>
+                <input
+                  id="firstName"
+                  type="text"
+                  placeholder="Enter your first name" className='border-2 p-2 rounded-md'
+                  {...register('firstName', {
+                    required: 'Firstname is required'
+                    //minlength: 2
+                  })}
+                />
+                {errors.firstName && (
+                  <p className='text-red-500'>{errors.firstName.message}</p>
+                )}
+              </div>
+
+
+              <div className='flex gap-x-6 w-15 mt-5  text-white pl-2'>
+                <label htmlFor="lastName" className="font-semibold">Lastname :</label>
+                <input
+                  id="lastName"
+                  type="text"
+                  placeholder="Enter your last name" className='border-2 p-2 rounded-md'
+                  {...register('lastName', {
+                    required: 'Lastname is required'
+                    //minlength: 8
+                  })}
+                />
+                {errors.lastname && (
+                  <p className='text-red-500'>{errors.lastName.message}</p>)}
+              </div>
+
+              <div className='flex gap-x-6 w-15 mt-5 font-semibold  text-white pl-7'>
+                <label htmlFor="email">Email :</label>
+                <input
+                  id='email'
+                  type="text"
+                  placeholder="Enter your email" className='border-2 p-2 rounded-md'
+                  {...register('email', {
+                    required: 'Email is required'
+                    //minlength: 8
+                  })}
+                />
+                {errors.email && (
+                  <p className='text-white'>{errors.email.message}</p>)}
+
+              </div>
+
+              <div className='flex gap-x-6 w-15 mt-5 font-semibold  text-white'>
+                <label htmlFor="password">Password :</label>
+                <input
+                  id='password'
+                  type="password"
+                  placeholder="Enter your password" className='border-2 p-2 rounded-md'
+                  {...register('password', {
+                    required: 'Password is required'
+                    //minlength: 8
+                  })}
+                />
+                {errors.password && (
+                  <p className='text-white'>{errors.password.message}</p>)}
+
+              </div>
+              <div className='flex flex-col'>
+                <button type="submit" className='bg-white w-15 p-2 
+        font-semibold border-2 rounded-md mt-5'>
+                  Signup
+                </button>
+                
+                {/* <label>
+                  <input
+                    type="checkbox" checked name="remember" />
+                  Remember me
+                </label> */}
+              </div>
+
+              <div className='flex flex-col w-15'>
+                <p>Already have an account?</p>
+                <div className="border-2 rounded-md p-2 text-center font-semibold bg-white">
+                  <Link to="/login">
+                    Login
+                  </Link>
+                </div>
+              </div>
+
+
+              <div className="border-2 rounded-sm p-2 text-center bg-white font-semibold mt-5" >
+                <button type="button">Cancel</button>
+              </div>
+              
+            </div>
+          </form>
+        </div>
       </div>
-      <div className='flex flex-col w-1/2'>
-        <div className="flex gap-x-6 w-full">
-          <label htmlFor="firstName" className="font-semibold">First name</label>
-          <input
-            id="firstName"
-            type="text"
-            placeholder="Enter your first name" className='border-2 p-2 rounded-md'
-            {...register('firstName', {
-              required: 'Firstname is required'
-              //minlength: 2
-            })}
-          />
-          {errors.firstName && (
-            <p className='text-blue-700'>{errors.firstName.message}</p>
-          )}
-        </div>
 
-
-        <div className=''>
-          <label htmlFor="lastName" className="font-semibold">Lastname</label>
-          <input
-            id="lastName"
-            type="text"
-            placeholder="Enter your last name" className='border-2 p-2 rounded-md'
-            {...register('lastName', {
-              required: 'Lastname is required'
-              //minlength: 8
-            })}
-          />
-          {errors.lastname && (<p className='text-red-500'>errors.lastName.message</p>)}
-        </div>
-        <div>
-          <label htmlFor="email">Email</label>
-          <input
-            id='email'
-            type="text" />
-        </div>
-        <div>
-          <label htmlFor="password">Password</label>
-          <input
-            id='password'
-            type="password" />
-
-
-
-
-        </div>
-
-
-        <button type="submit" className='bg-transparent text-white w-full p-2 rounded-md font-semibold'>
-          Signup
-        </button>
-
-
-        {/* <label>
-          <input
-            type="checkbox" checked name="remember" />
-          Remember me
-        </label> */}
-        <div>
-          <p>Already have an account?</p>
-          {/* <Link to="/login">
-        Login
-        </Link> */}
-        </div>
-
-
-        <div className="container" >
-          <button type="button" className="cancelbtn">Cancel</button>
-          <span className="">Forgot <a href="#">password?</a></span>
-        </div>
-       
-      </div>
-    </form>
+    </div>
   );
 };
 
