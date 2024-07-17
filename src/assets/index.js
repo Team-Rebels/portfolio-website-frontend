206a98fa63e64e078b1df131591930dfa76397ed
emmie-j <attachiegrace408@gmail.com>
2024-07-17 13:49:04 +0000

    login page has been uptated

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,31 +1,12 @@
 import banner2 from './images/banner2.png'
-
-export {
-    banner2
-};
-
 import profile from './images/profile.jpg'
-
-export {
-    profile
-};
-
 import website from './images/website.jpg'
-
-export {
-    website
-};
-
 import website5 from './images/website5.jpg'
-
-export {
-    website5
-};
-
 import website4 from './images/website4.jpg'
 
 export {
-    website4
+    banner2, profile, website, website5, website4
 };
+
 
 
